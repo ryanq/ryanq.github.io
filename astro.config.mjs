87c9155bf76049c66cf0ee3011e0c3a059bbd2b5
Ryan Quattlebaum <ryan.quattlebaum@icloud.com>
2025-07-04 10:47:13 -0400

    update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,4 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ryanq.github.io',
-  experimental: {
-    contentLayer: true,
-  },
 })
